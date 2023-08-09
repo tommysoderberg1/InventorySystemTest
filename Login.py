15a8c29f399c85705657712cdd095413c5640a54
tommysoderberg1 <104102609+tommysoderberg1@users.noreply.github.com>
2023-08-09 02:51:03 +0200

    First draft login

--- Login.py
+++ Login.py
@@ -1,6 +1,6 @@
 import customtkinter
+from tkinter import messagebox
 import sqlite3
-
 
 
 customtkinter.set_appearance_mode("Dark")
@@ -10,7 +10,8 @@ customtkinter.set_default_color_theme("dark-blue")
 class Login(customtkinter.CTk):
 	def __init__(self, title, size):
 		super().__init__()
-
+		
+		
 		self.title(title)
 		self.geometry(f'{size[0]}x{size[1]}')
 		self.minsize(size[0],size[1])
@@ -37,7 +38,7 @@ class LoginFrame(customtkinter.CTkFrame):
 			self.emailEntry = customtkinter.CTkEntry(self, placeholder_text="Email")
 			self.loginButton = customtkinter.CTkButton(self, text= "Login", command = self.loginCommand)
 			self.registerButton = customtkinter.CTkButton(self, text= "Register", command = self.registerCommand)
-			self.forgotPwButton = customtkinter.CTkButton(self, text= "Forgot password")
+			self.forgotPwButton = customtkinter.CTkButton(self, text= "Forgot password", command = self.forgotPwCommand)
 			self.checkbox = customtkinter.CTkCheckBox(self, text="Remember me")
 			
 			# place widgets
@@ -50,7 +51,9 @@ class LoginFrame(customtkinter.CTkFrame):
 			self.registerButton.pack(pady=10, padx=10)
 			self.forgotPwButton.pack(pady=10, padx=10)
 			self.checkbox.pack(padx=12, pady=10)
-
+		def forgotPwCommand(self):
+			messagebox.showinfo("","Not my problem")
+		
 		def loginCommand(self):
 			username = self.usernameEntry.get()
 			password = self.passwordEntry.get()
@@ -58,26 +61,41 @@ class LoginFrame(customtkinter.CTkFrame):
 			self.login(username, password) 
 		
 		def login(self, username : str, password : str):
-		# Create Table
+			
+			# create Table
 			self.conn = sqlite3.connect('login.db')
 			self.cursor = self.conn.cursor()	
 			
+			self.table_create_query = '''CREATE TABLE IF NOT EXISTS Login_Data 
+						(usernameSql TEXT, passwordSql TEXT, emailSql TEXT, userIdSql TEXT, userLevelSql TEXT)'''
+			self.conn.execute(self.table_create_query)
+			
+			# create admin
+			self.cursor.execute("SELECT COUNT(*) FROM Login_Data")
+			numberOfRows = str(self.cursor.fetchone())
+			print(numberOfRows)
+
+			if numberOfRows == "(0,)": 
+				
+				self.data_insert_query = '''INSERT INTO Login_Data (usernameSql, passwordSql, emailSql, 
+						userIdSql, userLevelSql) VALUES (?, ?, ?, ?, ?)'''
+				self.data_insert_tuple = ("Tommy", "password", "admin@email.com", "100001", "King")
+				self.cursor.execute(self.data_insert_query, self.data_insert_tuple)
+
+			# verify input for login and login if criteria is met
 			if username and password:
 
-				self.table_create_query = '''CREATE TABLE IF NOT EXISTS Login_Data 
-						(usernameSql TEXT, passwordSql TEXT, emailSql TEXT, userIdSql TEXT)'''
-				
 				self.conn.execute(self.table_create_query)
 				self.cursor.execute("SELECT * FROM Login_Data WHERE usernameSql = ?",(username,))
 				nameAndPw = str(self.cursor.fetchone())
 
 				if nameAndPw == "None": 
-					print("Account does not exist, register a new account")
+					messagebox.showinfo("","Account does not exist, register a new account")
 				else:
-					print(nameAndPw)
+					messagebox.showinfo("You are being logged in")
 					
 			else:
-				print("You need to type in both username and password!")
+				messagebox.showinfo("","You need to type in both username and password!")
 					
 			self.conn.commit()
 			self.conn.close()	
@@ -95,13 +113,11 @@ class LoginFrame(customtkinter.CTkFrame):
 			self.conn = sqlite3.connect('login.db')
 			self.cursor = self.conn.cursor()	
 			
+			# verify input for registration and register account if criteria is met.
 			if username and password and email:
 
 				self.table_create_query = '''CREATE TABLE IF NOT EXISTS Login_Data 
-						(usernameSql TEXT, passwordSql TEXT, emailSql TEXT, userIdSql TEXT)'''
-				# Create admin
-				self.data_insert_query = '''INSERT INTO Login_Data (usernameSql, passwordSql, 
-						emailSql, userIdSql) VALUES (Admin, Admin, Admin@gmail.com, 100001)'''
+						(usernameSql TEXT, passwordSql TEXT, emailSql TEXT, userIdSql TEXT, userLevelSql TEXT)'''
 				
 
 				self.conn.execute(self.table_create_query)
@@ -123,31 +139,34 @@ class LoginFrame(customtkinter.CTkFrame):
 					emailExist = True
 
 
-				if emailSqlRow == usernameRowSql: 
-					print("This account already exist!")
+				if emailSqlRow == usernameRowSql and emailExist and usernameExist: 
+					messagebox.showinfo("","This account already exist!")
 					
 				elif emailExist:
-					print("This email already has an account bound to it, choose another!")
+					messagebox.showinfo("","This email already has an account bound to it, choose another!")
 
 				elif usernameExist: 
-					print("The username you typed in is taken, choose another!")
+					messagebox.showinfo("","The username you typed in is taken, choose another!")
 				else:
 					self.cursor.execute("SELECT COUNT(*) FROM Login_Data")
 					numberOfRows = str(self.cursor.fetchone())
 					s1="".join(c for c in numberOfRows if c.isalnum())
 					userID : str = 100001 + int(s1)
 
-					self.data_insert_query = '''INSERT INTO Login_Data (usernameSql, passwordSql, emailSql, userIdSql) VALUES (?, ?, ?, ?)'''
-					self.data_insert_tuple = (username, password, email, userID)
+					self.data_insert_query = '''INSERT INTO Login_Data (usernameSql, passwordSql, 
+							emailSql, userIdSql, userLevelSql) VALUES (?, ?, ?, ?, ?)'''
+					self.data_insert_tuple = (username, password, email, userID, "Pawn")
 
 					self.cursor.execute(self.data_insert_query, self.data_insert_tuple)
-					print("New account created!")
+					messagebox.showinfo("","New account created!")
 
 			else:
-				print("You need to type in username, email and password to create an account!")
+				messagebox.showinfo("","You need to type in username, email and password to create an account!")
 					
 			self.conn.commit()
 			self.conn.close()
+		# def myMessageBox():
+		# 	messagebox.showinfo()
 
 
 Login('Login', (854,480))
